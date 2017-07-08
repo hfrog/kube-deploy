f4d6ad49f382b4732cc4c57fe78aeab825c0315d
Andrew Ivanov <and@hfrog.ru>
2017-07-08 20:08:23 +0000

    cni-plugin.sh rewrite

--- cni-plugin.sh
+++ cni-plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: set sw=2 :
 
 # Copyright 2016 The Kubernetes Authors All rights reserved.
 #
@@ -15,50 +16,73 @@
 # limitations under the License.
 
 # Utility functions for Kubernetes in docker setup and for cni network plugin.
-kube::cni::ensure_docker_settings(){
+
+kube::cni::docker_conf() {
+  systemctl cat docker | awk 'FNR==1 {print $2}'
+}
+
+kube::cni::restart_docker() {
+  systemctl daemon-reload
+  systemctl restart docker
+  kube::log::status "Restarted docker with service file modification(s)"
+}
+
+kube::cni::place_drop_in() {
+  local file=$1
+
+  kube::util::assure_dir $(dirname $file)
+  cat > $file.new
+  if [[ -f $file && -z $(diff $file $file.new) ]]; then
+    # drop-in file already exists and is the same as new, don't restart docker
+    rm -f $file.new
+  else
+    # install new file and restart docker
+    mv -f $file.new $file
+    restart=true
+    kube::log::status "Modified docker drop-in $file"
+  fi
+}
+
+kube::cni::ensure_docker_settings() {
 
   if kube::helpers::command_exists systemctl; then
-    local restart=false
-    DOCKER_CONF=$(systemctl cat docker | awk 'FNR==1 {print $2}')
+    restart=false
+    local conf=$(kube::cni::docker_conf)
 
     # Clear mtu and bip when previously started in docker-bootstrap mode
-    if [[ ! -z $(grep "mtu=" ${DOCKER_CONF}) && ! -z $(grep "bip=" ${DOCKER_CONF}) ]]; then
-      sed -i 's/--mtu=.* --bip=.*//g' ${DOCKER_CONF}
+    if [[ ! -z $(grep "mtu=" $conf) && ! -z $(grep "bip=" $conf) ]]; then
+      sed -i 's/--mtu=.* --bip=.*//g' $conf
       restart=true
       kube::log::status "The mtu and bip parameters removed"
     fi
 
-    # If we can find MountFlags but not MountFlags=shared, set MountFlags to shared
-    if [[ ! -z $(grep "MountFlags" ${DOCKER_CONF}) && -z $(grep -w "MountFlags=shared" ${DOCKER_CONF}) ]]; then
+    local drop_in_dir=/etc/systemd/system/docker.service.d
 
-      DOCKER_SERVICE_DIR="/etc/systemd/system/docker.service.d"
-      SHARED_MOUNTS_CONF="${DOCKER_SERVICE_DIR}/shared-mounts.conf"
+    local execstart=$(grep '^ExecStart=[^[:space:]]' $conf \
+        | sed 's/\(dockerd[[:space:]]\).*$/\1/')
+    kube::cni::place_drop_in $drop_in_dir/execstart.conf <<EOF
+[Service]
+ExecStart=
+$execstart --host=fd:// -H unix:///var/run/docker.sock -H tcp://127.0.0.1:4243 --ip-forward=true --iptables=true --raw-logs --ip-masq=true --selinux-enabled
+EOF
 
-      # Make a dropin file for shared mounts, as /usr/lib isn't always writeable
-      mkdir -p ${DOCKER_SERVICE_DIR}
-      cat > "${SHARED_MOUNTS_CONF}.new" <<EOF
+    # https://docs.docker.com/engine/userguide/networking/default_network/container-communication/#container-communication-between-hosts
+    # In Docker 1.12 and earlier, the default FORWARD chain policy was ACCEPT
+    kube::cni::place_drop_in $drop_in_dir/iptables-forward-accept.conf <<EOF
+[Service]
+ExecStartPost=/sbin/iptables -P FORWARD ACCEPT
+EOF
+
+    # Make a drop-in file for shared mounts, as /usr/lib isn't always writeable
+    kube::cni::place_drop_in $drop_in_dir/shared-mounts.conf <<EOF
 [Service]
 MountFlags=
 MountFlags=shared
 EOF
-      if [[ -f "${SHARED_MOUNTS_CONF}" && -z $(diff "${SHARED_MOUNTS_CONF}" "${SHARED_MOUNTS_CONF}.new") ]]; then
-        # shared mounts config file already exists and the same as new, don't restart docker
-        rm -f "${SHARED_MOUNTS_CONF}.new"
-      else
-        # install new file and restart docker
-        mv -f "${SHARED_MOUNTS_CONF}.new" "${SHARED_MOUNTS_CONF}"
-        restart=true
-
-        kube::log::status "systemd MountFlags option is now set to shared"
-      fi
-    fi
 
     # Check if restart needed
-    if [[ ${restart} == true ]]; then
-      systemctl daemon-reload
-      systemctl restart docker
-      kube::log::status "Restarted docker with service file modification"
+    if [[ $restart == true ]]; then
+      kube::cni::restart_docker
     fi
-
   fi
 }
